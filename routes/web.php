38e54ff758a7c59daedbff292442d5061daee80f
DevKandil <abdelrazek1337@gmail.com>
2023-08-28 12:03:49 +0300

    Episode #41

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,18 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show'])->where('post',
 
 
 
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
+// Route::get('categories/{category:slug}', function (Category $category) {
+//     return view('posts', [
+//         'posts' => $category->posts,
+//         'currentCategory' => $category,
+//         'categories' => Category::all()
+//     ]);
+// })->name('category');
 
 
 
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all()
-    ]);
-})->name('author');
+// Route::get('authors/{author:username}', function (User $author) {
+//     return view('posts.index', [
+//         'posts' => $author->posts,
+//     ]);
+// })->name('author');
